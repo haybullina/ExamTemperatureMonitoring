18f0c226306358964c80a6348f76a588e444fa72
haybullina <101605765+haybullina@users.noreply.github.com>
2022-06-25 10:57:47 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExamTemperatureMonitoring
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.FileName = "Document";
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "Text documents (.txt)|*.txt";
+
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                // Open document
+                string pathName = dlg.FileName;
+                using (StreamReader file = new StreamReader(pathName))
+                {
+
+                    string temp = file.ReadLine();
+                    tbStartTime.Text = temp;
+                    temp = file.ReadLine();
+                    tbData.Text = temp;
+
+                }
+
+            }
+        }
+
+        private void btnReport_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.FileName = "Document";
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "Text documents (.txt)|*.txt";
+
+            Nullable<bool> result = dlg.ShowDialog();
+    
+            if (result == true)
+            {
+                // Save document
+                string filename = dlg.FileName;
+            }
+        }
+
+        private void btnReport_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (tbTempMin.Text != null || tbTempMin.Text != "" || tbTimeMax.Text != null || tbTimeMax.Text != "")
+                {
+                    Violation report = new Violation(tbFishName.Text, Convert.ToInt32(tbTempMax.Text), Convert.ToInt32(tbTimeMax.Text),
+                                                     Convert.ToInt32(tbTempMin.Text), Convert.ToInt32(tbTimeMin.Text),
+                                                     tbData.Text, tbStartTime.Text);
+                    report.Pursing();
+                }
+                else 
+                {
+                    Violation report = new Violation(tbFishName.Text, Convert.ToInt32(tbTempMax.Text), Convert.ToInt32(tbTimeMax.Text),
+                                                     tbData.Text, tbStartTime.Text);
+                    report.Pursing();
+                }
+            }
+            catch 
+            {
+                string messageBoxText = "Остались пустые поля либо введены некорректные данные";
+                string caption = "Ошибка";
+                MessageBoxButton button = MessageBoxButton.OK;
+                MessageBoxImage icon = MessageBoxImage.Warning;
+                MessageBoxResult result;
+
+                result = MessageBox.Show(messageBoxText, caption, button, icon, MessageBoxResult.Yes);
+            }
+        }
+    }
+}
---
+++ Violation.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamTemperatureMonitoring
+{
+    internal class Violation
+    {
+        private string fishName;
+        private int tempMax;
+        private int time1;
+        private int tempMin = 0;
+        private int time2 = 0;
+        private string data;
+        private string startTime;
+
+        public Violation(string fishName, int tempMax, int time1, int tempMin, int time2, string data, string startTime)
+        {
+            this.tempMax = tempMax;
+            this.time1 = time1;
+            this.tempMin = tempMin;
+            this.time2 = time2;
+            this.data = data;
+            this.startTime = startTime;
+        }
+
+        public Violation(string fishName, int tempMax, int time1, string data, string startTime)
+        {
+            this.tempMax = tempMax;
+            this.time1 = time1;
+            this.data = data;
+            this.startTime = startTime;
+        }
+
+        public string Pursing() 
+        {
+            string result = "";
+
+            if (tempMin != 0 || time2 != 0) 
+            {
+                List<int> temperature = new List<int>();
+
+                string[] temps = data.Split();
+                foreach (string temp in temps)
+                {
+                    temperature.Add(Convert.ToInt32(temp));
+                }
+                
+
+            }
+
+            return result;
+        }
+
+    }
+}
